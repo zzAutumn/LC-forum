ae8c854b5a5901d6e6cf6b0c991ddde13d4babbe
yezi <jingjing.ye@dragonsoftbravo.com>
2018-10-27 21:50:19 +0800

    create project

---
+++ frontend-nuxt/layouts/default.vue
@@ -0,0 +1,27 @@
+/*
+ * @Author: yezi
+ * @Date: 2018-10-27 21:47:16
+ * @Last Modified by:   yezi
+ * @Last Modified time: 2018-10-27 21:47:16
+ * 项目模板布局
+ */
+
+<template>
+  <v-app>
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  }
+}
+</script>
