060df3016b84485147935943a431007437b774ba
yezi <jingjing.ye@dragonsoftbravo.com>
2018-11-01 06:29:00 +0800

    u

--- frontend-nuxt/pages/demo.vue
+++ frontend-nuxt/pages/demo.vue
@@ -19,7 +19,6 @@
         <v-flex class="search-box">
           <v-text-field
             dense
-            single-line
             append-icon="search"
             height=42
             placeholder="I'm looking for a product..."
@@ -37,12 +36,67 @@
         </v-flex>
       </v-layout>
     </v-toolbar>
+     <v-divider></v-divider>
+     <v-toolbar flat color="#fff" dense>
+       <v-layout>
+         <v-flex></v-flex>
+         <v-flex>
+           <v-layout>
+              <v-flex>Chrismas</v-flex>
+              <v-flex>New</v-flex>
+              <v-flex>Sofas</v-flex>
+              <v-flex>Furniture</v-flex>
+              <v-flex>Decor</v-flex>
+              <v-flex>Lighting</v-flex>
+              <v-flex>Junior</v-flex>
+              <v-flex>Styles</v-flex>
+              <v-flex>Inspiration</v-flex>
+              <v-flex>Pro</v-flex>
+           </v-layout>
+         </v-flex>
+         <v-flex></v-flex>
+       </v-layout>
+     </v-toolbar>
+
+     <!-- 图片swiper区 -->
+     <v-layout class="product-area">
+       <v-flex>
+         <v-layout>
+           <v-flex><div class="circular"></div></v-flex>
+           <v-flex>
+             <v-carousel>
+                <v-carousel-item
+                  v-for="(item,i) in items"
+                  :key="i"
+                  :src="item.src"
+                ></v-carousel-item>
+              </v-carousel>
+           </v-flex>
+           <v-flex></v-flex>
+         </v-layout>
+       </v-flex>
+       <v-flex></v-flex>
+     </v-layout>
   </div>
 </template>
 
 <script>
 export default {
-
+  data() {
+    return {
+      items: [
+        {
+          src: () => import('@/assets/images/sofa1.png')
+        },
+        {
+          src: () => import('@/assets/images/sofa2.png')
+        },
+        {
+          src: () => import('@/assets/images/sofa3.png')
+        }
+      ]
+    }
+  }
 }
 </script>
 
@@ -77,6 +131,14 @@ export default {
     img{
       height: 42px;
     }
+  }
+}
+.product-area{
+  .circular{
+    height: 90px;
+    width: 90px;
+    background: #c65b51;
+    border-radius: 50%;
   }
 }
 </style>
